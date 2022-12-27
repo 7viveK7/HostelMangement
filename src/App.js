869ca64bd62bb50a9bead42303efc18ca5a83d1c
vivekananda <vivekanandamalladi9@gmail.com>
2022-12-27 23:30:05 +0530

    login ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { Button, Divider, Form, Grid, Segment } from "semantic-ui-react";
 
-function App() {
+const App = () => {
+  const [signUp, setSignUp] = useState(true);
+  const Signup = () => {
+    return (
+      <Form unstackable>
+        <Form.Group widths={2}>
+          <Form.Input label="First name" placeholder="First name" />
+          <Form.Input label="Last name" placeholder="Last name" />
+        </Form.Group>
+        <Form.Checkbox label="I agree to the Terms and Conditions" />
+        <Button type="submit">Submit</Button>
+        <Button type="button" primary onClick={() => setSignUp(!signUp)}>
+          Login
+        </Button>
+      </Form>
+    );
+  };
+
+  const Home = () => {
+    return (
+      <div>
+        <h1>hello user</h1>
+      </div>
+    );
+  };
+  const Login = () => {
+    const [input, setInput] = useState({ Username: "", password: "" });
+
+    return (
+      <>
+        <Segment placeholder>
+          <Grid columns={2} relaxed="very" stackable>
+            <Grid.Column>
+              <Form>
+                <Form.Input
+                  icon="user"
+                  iconPosition="left"
+                  label={input.Username}
+                  placeholder="Username"
+                  onChange={(e) =>
+                    setInput({ ...input, Username: e.target.value })
+                  }
+                />
+                <Form.Input
+                  icon="lock"
+                  iconPosition="left"
+                  label="Password"
+                  type="password"
+                  onChange={(e) =>
+                    setInput({ ...input, password: e.target.value })
+                  }
+                />
+
+                <Button content="Login" primary />
+              </Form>
+            </Grid.Column>
+            <Grid.Column verticalAlign="middle">
+              <Button
+                content="Sign up"
+                icon="signup"
+                size="big"
+                onClick={() => setSignUp(!signUp)}
+              />
+            </Grid.Column>
+          </Grid>
+        </Segment>
+      </>
+    );
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="loginContainer">{signUp ? <Login /> : <Signup />}</div>
   );
-}
+};
 
 export default App;
